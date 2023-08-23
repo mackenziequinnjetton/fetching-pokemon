595ca79d65a804c68725b8ae0277e06d867d8f32
MacKenzie Quinn Jetton <78572140+mackenziequinnjetton@users.noreply.github.com>
2023-08-23 11:14:51 -0600

    Render PokemonInformation to App page

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -5,6 +5,7 @@ test('renders expected number of elements', () => {
   render(<App />);
   const elements1 = screen.getAllByText(/./i);
   const elements2 = screen.getAllByPlaceholderText(/./i);
-  const allElements = [...elements1, ...elements2]
-  expect(allElements).toHaveLength(2);
+  const elements3 = screen.getAllByAltText(/./i);
+  const allElements = [...elements1, ...elements2, ...elements3]
+  expect(allElements).toHaveLength(9);
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css';
+import { PokemonInformation } from './components/PokemonInformation';
 import { PokemonSearch } from './components/PokemonSearch';
 
 function App() {
   return (
     <>
       <PokemonSearch />
+      <PokemonInformation />
     </>
   );
 }
