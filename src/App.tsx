db2f487d25b2faf61a790d2d732fbafd4d2d2b15
MacKenzie Quinn Jetton <78572140+mackenziequinnjetton@users.noreply.github.com>
2023-08-24 06:53:03 -0600

    Install and configure react-query-devtools

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import './App.css';
 import { PokemonInformation } from './components/PokemonInformation';
 import { PokemonSearch } from './components/PokemonSearch';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <PokemonSearch />
       <PokemonInformation />
-    </>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 }
 
