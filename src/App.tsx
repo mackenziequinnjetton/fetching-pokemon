11755268ecdbc8ec1bafe49db57de7578d91908f
MacKenzie Quinn Jetton <78572140+mackenziequinnjetton@users.noreply.github.com>
2023-08-24 09:57:45 -0600

    WIP implement gql query support

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,39 @@
 import './App.css';
+import { graphql } from './gql/gql';
 import { PokemonInformation } from './components/PokemonInformation';
 import { PokemonSearch } from './components/PokemonSearch';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { useQuery } from '@tanstack/react-query';
+import { request } from 'graphql-request';
 
-const queryClient = new QueryClient();
+const getPokemonQueryDocument = graphql(/* GraphQL */ `
+  query getPokemonQuery($name: String!) {
+    pokemon(name: $name) {
+      number
+      name
+      attacks {
+        special {
+          name
+          type
+          damage
+        }
+      }
+    }
+  }
+`)
 
 function App() {
+  const { data } = useQuery({
+    queryKey: ['pokemon'],
+    queryFn: async () => {
+      request('https://graphql-pokemon2.vercel.app/', getPokemonQueryDocument, { name: 'Pikachu' })
+    }
+  });
+
   return (
-    <QueryClientProvider client={queryClient}>
+    <>
       <PokemonSearch />
       <PokemonInformation />
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
+    </>
   );
 }
 
