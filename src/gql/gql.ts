f0d59835b29e9d40c1386b9f0ffbe0e197c3e15a
MacKenzie Quinn Jetton <78572140+mackenziequinnjetton@users.noreply.github.com>
2023-08-24 14:52:59 -0600

    Update graphql type definitions

--- src/gql/gql.ts
+++ src/gql/gql.ts
@@ -13,7 +13,7 @@ import { TypedDocumentNode as DocumentNode } from '@graphql-typed-document-node/
  * Therefore it is highly recommended to use the babel or swc plugin for production.
  */
 const documents = {
-    "\n  query getPokemonQuery($name: String!) {\n    pokemon(name: $name) {\n      number\n      name\n      attacks {\n        special {\n          name\n          type\n          damage\n        }\n      }\n    }\n  }\n": types.GetPokemonQueryDocument,
+    "\n  query getPokemon($name: String!) {\n    pokemon(name: $name) {\n      name\n      number\n      image\n      attacks {\n        special {\n          name\n          type\n          damage\n        }\n      }\n    }\n  }\n": types.GetPokemonDocument,
 };
 
 /**
@@ -33,7 +33,7 @@ export function graphql(source: string): unknown;
 /**
  * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
  */
-export function graphql(source: "\n  query getPokemonQuery($name: String!) {\n    pokemon(name: $name) {\n      number\n      name\n      attacks {\n        special {\n          name\n          type\n          damage\n        }\n      }\n    }\n  }\n"): (typeof documents)["\n  query getPokemonQuery($name: String!) {\n    pokemon(name: $name) {\n      number\n      name\n      attacks {\n        special {\n          name\n          type\n          damage\n        }\n      }\n    }\n  }\n"];
+export function graphql(source: "\n  query getPokemon($name: String!) {\n    pokemon(name: $name) {\n      name\n      number\n      image\n      attacks {\n        special {\n          name\n          type\n          damage\n        }\n      }\n    }\n  }\n"): (typeof documents)["\n  query getPokemon($name: String!) {\n    pokemon(name: $name) {\n      name\n      number\n      image\n      attacks {\n        special {\n          name\n          type\n          damage\n        }\n      }\n    }\n  }\n"];
 
 export function graphql(source: string) {
   return (documents as any)[source] ?? {};
