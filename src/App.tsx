9536fa533e9cd8ef23d4733bb2763533956ad0f6
MacKenzie Quinn Jetton <78572140+mackenziequinnjetton@users.noreply.github.com>
2023-08-28 03:40:36 -0600

    Remove comments

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,7 @@
 import './App.css';
 import { PokemonSearch } from './components/PokemonSearch';
 
-// const getPokemonDocument = graphql(/* GraphQL */ `
-//   query getPokemon($name: String!) {
-//     pokemon(name: $name) {
-//       name
-//       number
-//       image
-//       attacks {
-//         special {
-//           name
-//           type
-//           damage
-//         }
-//       }
-//     }
-//   }
-// `)
-
 function App() {
-  // const { data } = useQuery({
-  //   queryKey: ['pokemon'],
-  //   queryFn: async () => {
-  //     return await request(
-  //       'https://graphql-pokemon2.vercel.app/?query=&operationName=getPokemon', 
-  //       getPokemonDocument, 
-  //       { name: 'Pikachu' })
-  //   }
-  // });
-
   return (
     <>
       <PokemonSearch />
