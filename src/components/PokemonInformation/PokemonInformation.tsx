008f452f52246e3a8c7f2edf0057124127b8d552
MacKenzie Quinn Jetton <78572140+mackenziequinnjetton@users.noreply.github.com>
2023-08-29 03:19:59 -0600

    Make placeholder image element disappear once image is rendered

--- src/components/PokemonInformation/PokemonInformation.tsx
+++ src/components/PokemonInformation/PokemonInformation.tsx
@@ -7,11 +7,11 @@ const PokemonInformation = ({ pokemon }: { pokemon: Pokemon }) => {
       <h1 id="pokemon-name">{pokemon?.name ? pokemon.name : "No Pokemon Yet!"}</h1>
       <p id="pokemon-number">({pokemon?.number ? pokemon.number : "xxx"})</p>
       <div>
-        <p id="pokemon-image-placeholder">Please submit a Pokemon!</p>
+        <p id="pokemon-image-placeholder">{pokemon?.image ? "" : "Please submit a Pokemon!"}</p>
         <img 
           id="pokemon-image"
-          alt="Your searched Pokemon."
-          src={pokemon?.image ? pokemon?.image : ""} 
+          alt={pokemon?.image ? "Your searched Pokemon." : ""}
+          src={pokemon?.image ?? ""} 
         />
       </div>
       <table id="pokemon-moves">
